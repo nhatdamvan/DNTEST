84faa743c84e7e135645900598260df8e732e22c
Nhat Dam Van <damnhat9308@gmail.com>
2025-09-14 16:44:56 +0700

    fixbug

--- server/app.js
+++ server/app.js
@@ -61,7 +61,7 @@ app.use(limiter);
 
 // CORS
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: "*",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
